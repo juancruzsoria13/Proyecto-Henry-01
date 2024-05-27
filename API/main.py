b3961d090b78938b4c7d68aad07d046f0425afbd
juancruzsoria13 <juancruz.soria@gmail.com>
2024-05-27 20:22:21 -0300

    Repo

---
+++ API/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+
+
+
+app= FastAPI()
+
+@app.get("/")
+
+def mensaje():
+    return ("Bienvenido a la API de datos de steam")
+#Consulta 1
+@app.get("/developer/")
+
+
+
+
+def desarrollador(developer:str):
+    filtro_developer= df[df["developer"] == developer]
+    cantidad_items=filtro_developer["id"].count()
+    free_por_año= filtro_developer.groupby("release_date").agg({
+        "id": "count",
+        'price': lambda x: ((x == 0.00).mean() * 100)
+        
+    }).reset_index()
+    free_por_año.rename(columns={"id":"cantidad_items"},inplace=True)
+    free_por_año.rename(columns={"price":"contenido_free"},inplace=True)
+    free_por_año.rename(columns={"release_date":"año"},inplace=True)
+    free_por_año[["año","cantidad_items","contenido_free"]]
+    free_por_año["contenido_free"]=free_por_año["contenido_free"].astype(str) + "%"
+    
+    return free_por_año
+
+
+developer=input("Ingrese el desarrollador")
+desarrollador(developer)
