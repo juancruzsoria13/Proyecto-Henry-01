556dca3e4bf15bc63cd0fd985dfc11fc7228c75f
juancruzsoria13 <juancruz.soria@gmail.com>
2024-05-28 18:45:22 -0300

    m

--- API/main.py
+++ API/main.py
@@ -9,10 +9,14 @@ app = FastAPI()
 def mensaje():
     return {"mensaje": "Bienvenido a la API de datos de steam"}
 
+
+default_parquet_path = r"C:\Users\juanc.DESKTOP-LGMDQP1\OneDrive\Documentos\Proyecto Henry 01\Datos Limpios\output_games_clean.parquet"
+parquet_path = os.getenv("parquet_path", default_parquet_path)
+
 # Consulta 1
 @app.get("/developer/")
 def desarrollador(developer: str):
-    df = pd.read_parquet(r"C:\Users\juanc.DESKTOP-LGMDQP1\OneDrive\Documentos\Proyecto Henry 01\Datos Limpios\output_games_clean.parquet")
+    df = pd.read_parquet(parquet_path)
     filtro_developer = df[df["developer"] == developer]
     cantidad_items = filtro_developer["id"].count()
     
