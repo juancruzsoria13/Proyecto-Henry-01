36faceb40486c78a5bf17aa06fe76b69b5c02598
juancruzsoria13 <juancruz.soria@gmail.com>
2024-05-28 14:07:34 -0300

    h

--- API/main.py
+++ API/main.py
@@ -1,36 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 import pandas as pd
+import uvicorn
 
-
-app= FastAPI()
+app = FastAPI()
 
 @app.get("/")
-
 def mensaje():
-    return ("Bienvenido a la API de datos de steam")
-#Consulta 1
+    return {"mensaje": "Bienvenido a la API de datos de steam"}
+
+# Consulta 1
 @app.get("/developer/")
-
-
-
-
-def desarrollador(developer:str):
+def desarrollador(developer: str):
     df = pd.read_parquet(r'C:\Users\juanc.DESKTOP-LGMDQP1\OneDrive\Documentos\Proyecto Henry 01\Datos Limpios\output_games_clean.parquet')
-    filtro_developer= df[df["developer"] == developer]
-    cantidad_items=filtro_developer["id"].count()
-    free_por_año= filtro_developer.groupby("release_date").agg({
+    filtro_developer = df[df["developer"] == developer]
+    cantidad_items = filtro_developer["id"].count()
+    
+    free_por_año = filtro_developer.groupby("release_date").agg({
         "id": "count",
         'price': lambda x: ((x == 0.00).mean() * 100)
-        
     }).reset_index()
-    free_por_año.rename(columns={"id":"cantidad_items"},inplace=True)
-    free_por_año.rename(columns={"price":"contenido_free"},inplace=True)
-    free_por_año.rename(columns={"release_date":"año"},inplace=True)
-    free_por_año[["año","cantidad_items","contenido_free"]]
-    free_por_año["contenido_free"]=free_por_año["contenido_free"].astype(str) + "%"
     
-    return free_por_año
+    free_por_año.rename(columns={"id": "cantidad_items", "price": "contenido_free", "release_date": "año"}, inplace=True)
+    free_por_año["contenido_free"] = free_por_año["contenido_free"].astype(str) + "%"
+    
+    return free_por_año[["año", "cantidad_items", "contenido_free"]].to_dict(orient="records")
 
-
-developer=input("Ingrese el desarrollador")
-desarrollador(developer)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
