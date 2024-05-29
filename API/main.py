d962652f43346ab7db28653da72bdde3b3d2c1b2
juancruzsoria13 <juancruz.soria@gmail.com>
2024-05-29 06:25:45 -0300

    Quinto commit

--- API/main.py
+++ API/main.py
@@ -1,26 +1,21 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
 import pandas as pd
 import uvicorn
-import os
 from sklearn.neighbors import NearestNeighbors
-import pandas as pd
+import os
 import numpy as np
 
 app = FastAPI()
 
-@app.get("/")
-def mensaje():
-    return {"mensaje": "Bienvenido a la API de datos de steam"}
-
 
 default_parquet_path = os.path.join("Datos Limpios", "output_games_clean.parquet")
 parquet_path = os.getenv("parquet_path", default_parquet_path)
+df_games = pd.read_parquet(parquet_path)
 
 # Consulta 1
 @app.get("/developer/")
 def desarrollador(developer: str):
-    df = pd.read_parquet(parquet_path)
-    filtro_developer = df[df["developer"] == developer]
+    filtro_developer = df_games[df_games["developer"] == developer]
     cantidad_items = filtro_developer["id"].count()
     
     free_por_año = filtro_developer.groupby("release_date").agg({
@@ -33,38 +28,27 @@ def desarrollador(developer: str):
     
     return free_por_año[["año", "cantidad_items", "contenido_free"]].to_dict(orient="records")
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
 
 
 
 
 
-
-#Modelo de recomendacion
-
-df_games= pd.read_parquet(parquet_path)
-# Manejo de valores faltantes: rellenar con cero o eliminar las filas con valores faltantes
-df_games_numeric = df_games.select_dtypes(include=[np.number]).fillna(0)  # Rellenar con cero
-
-# Inicializar el modelo NearestNeighbors
-nn_model = NearestNeighbors(metric='cosine', algorithm='brute')  # Utilizando la distancia coseno
-
-# Entrenar el modelo
+# Modelo de recomendación
+df_games_numeric = df_games.select_dtypes(include=[np.number]).fillna(0)
+nn_model = NearestNeighbors(metric='cosine', algorithm='brute')
 nn_model.fit(df_games_numeric)
 
 # Función para obtener las recomendaciones de juegos similares a uno dado
 @app.get("/juego/")
-def recomendacion_juego(product_name):
+def recomendacion_juego(product_name: str):
     try:
-        # Obtener el índice del producto en el DataFrame
         idx = df_games.index[df_games['app_name'] == product_name].tolist()[0]
-        # Calcular las distancias y los índices de los vecinos más cercanos
-        distances, indices = nn_model.kneighbors([df_games_numeric.iloc[idx]], n_neighbors=6)  # Excluimos el propio juego y tomamos los 5 más similares
-        # Obtener los nombres de los juegos más similares
-        similar_product_names = [df_games.iloc[i]['app_name'] for i in indices.flatten()[1:]]  # Excluimos el propio juego
+        distances, indices = nn_model.kneighbors([df_games_numeric.iloc[idx]], n_neighbors=6)
+        similar_product_names = [df_games.iloc[i]['app_name'] for i in indices.flatten()[1:]]
         return similar_product_names
     except IndexError:
         raise ValueError("El nombre de juego proporcionado no se encuentra en el DataFrame")
 
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
---
+++ Readme.md
@@ -0,0 +1,38 @@
+Proyecto MLOps: Sistema de Recomendación de Videojuegos para Usuarios en Steam
+Bienvenidos al proyecto MLOps de Machine Learning Operations (MLOps)
+
+Descripción del Problema
+En este proyecto se busca realizar la implementacion de un modelo predictivo para  hacer recomendaciones de juegos, basadas en juegos similares
+
+Contexto y Rol a Desarrollar
+Contexto
+Trabajamos en Steam como data scientist, una plataforma de videojuegos. Nuestro objetivo es crear un sistema de recomendación de videojuegos para usuarios.
+
+Rol a Desarrollar
+Actuamos como data engineer,data scientist y MLOps Engineer en Steam. 
+
+ETL
+Se realiza la extraccion y transformacion correcta de los datos para poder trabajarlos de manera sencilla
+
+
+
+Análisis exploratorio de los datos EDA
+Se realiza un analisis exploratorio con la finalidad de tener una idea clara de los datos
+
+Desarrollo API
+Se desarrolla una API que responda a las consultas realizadas
+
+Deployment
+Se utiliza el servicio de Render para poder hacer un deployment en la web
+
+Modelo de aprendizaje automático MODELO
+Se desarrolló un modelo de recomendación item-item para cumplir el objetivo final de realizar las recomendaciones correctas
+
+Requisitos
+fastapi
+uvicorn
+pandas
+sqlalchemy
+scikit-learn
+fastparquet
+numpy
