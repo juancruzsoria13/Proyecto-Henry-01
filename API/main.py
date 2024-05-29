477aecf624a90c803b4b746c5f2b99cf58fbc7b6
juancruzsoria13 <juancruz.soria@gmail.com>
2024-05-29 06:03:27 -0300

    Cuarto commit

--- API/main.py
+++ API/main.py
@@ -43,5 +43,28 @@ if __name__ == "__main__":
 
 #Modelo de recomendacion
 
+df_games= pd.read_parquet(parquet_path)
+# Manejo de valores faltantes: rellenar con cero o eliminar las filas con valores faltantes
+df_games_numeric = df_games.select_dtypes(include=[np.number]).fillna(0)  # Rellenar con cero
+
+# Inicializar el modelo NearestNeighbors
+nn_model = NearestNeighbors(metric='cosine', algorithm='brute')  # Utilizando la distancia coseno
+
+# Entrenar el modelo
+nn_model.fit(df_games_numeric)
+
+# Función para obtener las recomendaciones de juegos similares a uno dado
+@app.get("/juego/")
+def recomendacion_juego(product_name):
+    try:
+        # Obtener el índice del producto en el DataFrame
+        idx = df_games.index[df_games['app_name'] == product_name].tolist()[0]
+        # Calcular las distancias y los índices de los vecinos más cercanos
+        distances, indices = nn_model.kneighbors([df_games_numeric.iloc[idx]], n_neighbors=6)  # Excluimos el propio juego y tomamos los 5 más similares
+        # Obtener los nombres de los juegos más similares
+        similar_product_names = [df_games.iloc[i]['app_name'] for i in indices.flatten()[1:]]  # Excluimos el propio juego
+        return similar_product_names
+    except IndexError:
+        raise ValueError("El nombre de juego proporcionado no se encuentra en el DataFrame")
 
 
